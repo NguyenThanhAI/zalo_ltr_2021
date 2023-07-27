3d16ddef2b24c2879071cefa7cca24c50d910963
NguyenThanhAI <Thanh.CEAA.K58.HUST@gmail.com>
2023-07-27 12:37:03 +0700

    Add negative mining for mnrl

---
+++ mnrl_combined_negative_mining.py
@@ -0,0 +1,142 @@
+from typing import List, Dict
+
+import numpy as np
+import json
+import torch
+from tqdm import tqdm
+from rank_bm25 import *
+import argparse
+import os
+import pickle
+import glob
+from utils import bm25_tokenizer, calculate_f2
+
+from sentence_transformers import SentenceTransformer, util
+from utils import load_model, encode_legal_data, encode_question, load_bm25, load_encoded_legal_data
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--data_dir", type=str, default="zac2021_ltr_data")
+    parser.add_argument("--train_json_path", type=str, default="for_train_question_answer.json")
+    parser.add_argument("--legal_dict_json", default="generated_data/legal_dict.json", type=str)
+    parser.add_argument("--model_dir", default="saved_model", type=str)
+    parser.add_argument("--model_paths", default=None, type=str)
+    parser.add_argument("--top_k", default=25, type=int, help="top k hard negative mining")
+    parser.add_argument("--bm25_path", default="saved_model/bm25_Plus_04_06_model_full_manual_stopword", type=str)
+    parser.add_argument("--encoded_legal_data_path", default="encoded_data/encoded_legal_data.pkl", type=str)
+    parser.add_argument("--doc_refers_path", default="saved_model/doc_refers_saved", type=str)
+    parser.add_argument("--save_dir", default="pair_data", type=str)
+    
+    args = parser.parse_args()
+    
+    return args
+
+
+if __name__ == "__main__":
+    
+    args = get_args()
+    
+    data_dir = args.data_dir
+    train_json_path = args.train_json_path
+    legal_dict_json = args.legal_dict_json
+    model_dir = args.model_dir
+    model_paths = args.model_paths.split(",")
+    top_k = args.top_k
+    bm25_path = args.bm25_path
+    encoded_legal_data_path = args.encoded_legal_data_path
+    doc_refers_path = args.doc_refers_path
+    save_dir = args.save_dir
+    
+    if encoded_legal_data_path:
+        print("Load embedding of legal data")
+        emb_legal_data = load_encoded_legal_data(legal_data_path=encoded_legal_data_path)
+    else:
+        print("Compute embedding of legal data against models")
+        emb_legal_data: Dict[str, np.ndarray] = {}
+        for model_path in tqdm(model_paths):
+            print("Load model {}".format(model_path))
+            model = load_model(model_dir=args.model_dir, model_path=model_path)
+            print("Encode legal data using {}".format(model_path))
+            emb_legal_data[model_path] = encode_legal_data(args.legal_dict_json, model=model)
+            
+    train_path = os.path.join(data_dir, train_json_path)
+    data = json.load(open(train_path))
+    items = data["items"]
+    print("Num train question: {}".format(len(items)))
+    
+    print("Load BM25 model")
+    bm25 = load_bm25(bm25_path=bm25_path)
+    
+    print("Load doc refer")
+    with open(doc_refers_path, "rb") as doc_refer_file:
+        doc_refers = pickle.load(doc_refer_file)
+    
+    doc_path = os.path.join(legal_dict_json)
+    df = open(doc_path)
+    doc_data = json.load(df)
+    
+    print("Compute questions embedding against models")
+    question_emb_dict = {}
+    for model_path in tqdm(model_paths):
+        model = load_model(model_dir=model_dir, model_path=model_path)
+        question_emb_dict[model_path] = encode_question(questions=items, model=model)
+        
+    weights = [0.35, 0.15, 0.35, 0.15]
+    
+    save_triplets = []
+    
+    for idx, item in tqdm(enumerate(items)):
+        question_id = item["question_id"]
+        question = item["question"]
+        relevant_articles = item["relevant_articles"]
+        actual_positive = len(relevant_articles)
+        
+        # tokenized_query = bm25_tokenizer(question)
+        # doc_scores = bm25.get_scores(tokenized_query)
+        
+        pos_passage = []
+        
+        for article in relevant_articles:
+            concat_id = article["law_id"] + "_" + article["article_id"]
+            pos_passage.append(doc_data[concat_id]["title"] + " " + doc_data[concat_id]["text"])
+            break
+            
+        cos_sim = []
+        
+        for idx_2, model_path in enumerate(model_paths):
+            emb1 = question_emb_dict[model_path][question_id]
+            emb2 = emb_legal_data[model_path]
+            
+            scores = util.cos_sim(emb1, emb2)
+            
+            cos_sim.append(weights[idx_2] * scores)
+            
+        cos_sim = torch.cat(cos_sim, dim=0)
+        
+        cos_sim = torch.sum(cos_sim, dim=0).squeeze(0).numpy()
+        new_scores = cos_sim
+        predictions = np.argpartition(new_scores, len(new_scores) - top_k)[-top_k:]
+        
+        neg_passage = []
+        
+        for idx_2, idx_pred in enumerate(predictions):
+            pred = doc_refers[idx_pred]
+            
+            check = 0
+            for article in relevant_articles:
+                check += 1 if pred[0] == article["law_id"] and pred[1] == article["article_id"] else 0
+            
+            if check == 0:
+                neg_passage.append(doc_data[concat_id]["title"] + " " + doc_data[concat_id]["text"])
+                
+                
+        for pos in pos_passage:
+            for neg in neg_passage:
+                save_triplets.append({"question_id": question_id,
+                                      "triplets": (question, pos, neg)})
+    
+    os.makedirs(save_dir, exist_ok=True)
+    with open(os.path.join(save_dir, f"save_mnrl_combined_triplets_top{top_k}.pkl"), "wb") as triplet_file:
+        pickle.dump(save_triplets, triplet_file)
---
+++ mnrl_hard_negative_mining.py
@@ -0,0 +1,111 @@
+import pickle
+import os
+import numpy as np
+import json
+import torch
+from tqdm import tqdm
+from rank_bm25 import *
+import argparse
+import warnings 
+from sentence_transformers import SentenceTransformer, util
+warnings.filterwarnings("ignore")
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="phobert_base")
+    parser.add_argument("--model_path", default="saved_model/bm25_Plus_04_06_model_full_manual_stopword", type=str)
+    parser.add_argument("--sentence_bert_path", default="", type=str, help="path to round 1 sentence bert model")
+    parser.add_argument("--data_path", default="zac2021-ltr-data", type=str, help="path to input data")
+    parser.add_argument("--save_path", default="pair_data", type=str)
+    parser.add_argument("--top_k", default=20, type=int, help="top k hard negative mining")
+    parser.add_argument("--path_doc_refer", default="generated_data/doc_refers_saved.pkl", type=str, help="path to doc refers")
+    parser.add_argument("--path_legal", default="generated_data/legal_dict.json", type=str, help="path to legal dict")
+    args = parser.parse_args()
+    
+    
+    # load training data from json
+    data = json.load(open(os.path.join(args.data_path, "for_train_question_answer.json")))
+
+    training_data = data["items"]
+    print(len(training_data))
+
+    # load bm25 model
+    with open(args.model_path, "rb") as bm_file:
+        bm25 = pickle.load(bm_file)
+
+    with open(args.path_doc_refer, "rb") as doc_refer_file:
+        doc_refers = pickle.load(doc_refer_file)
+
+    doc_path = os.path.join(args.path_legal)
+    df = open(doc_path)
+    doc_data = json.load(df)
+    
+    # load hard negative model
+    model = SentenceTransformer(args.sentence_bert_path)
+
+    # add embedding for data
+    # if you already have data with encoded sentence uncoment line 47 - 54
+    import pickle
+    embed_list = []
+    for k, v in tqdm(doc_data.items()):
+        embed = model.encode(v['title'] + ' ' + v['text'])
+        doc_data[k]['embedding'] = embed
+
+    with open(os.path.join("generated_data", f'legal_corpus_{args.model_name}_embedding.pkl'), 'wb') as pkl:
+        pickle.dump(doc_data, pkl)
+
+    with open(os.path.join("generated_data", f'legal_corpus_{args.model_name}_embedding.pkl'), 'rb') as pkl:
+        data = pickle.load(pkl)
+        
+    top_k = args.top_k
+    save_triplets = []
+    
+    for idx, item in tqdm(enumerate(training_data)):
+        question_id = item["question_id"]
+        question = item["question"]
+        relevant_articles = item["relevant_articles"]
+        actual_positive = len(relevant_articles)
+        
+        pos_passage = []
+        
+        for article in relevant_articles:
+            concat_id = article["law_id"] + "_" + article["article_id"]
+            pos_passage.append(doc_data[concat_id]["title"] + " " + doc_data[concat_id]["text"])
+            break
+            
+        encoded_question  = model.encode(question)
+        list_embs = []
+        
+        
+        for k, v in data.items():
+            emb_2 = torch.tensor(v['embedding']).unsqueeze(0)
+            list_embs.append(emb_2)
+            
+        matrix_emb = torch.cat(list_embs, dim=0)
+        all_cosine = util.cos_sim(encoded_question, matrix_emb).numpy().squeeze(0)
+        predictions = np.argpartition(all_cosine, len(all_cosine) - top_k)[-top_k:]
+        
+        neg_passage = []
+        
+        for idx_2, idx_pred in enumerate(predictions):
+            pred = doc_refers[idx_pred]
+            
+            check = 0
+            for article in relevant_articles:
+                check += 1 if pred[0] == article["law_id"] and pred[1] == article["article_id"] else 0
+            
+            if check == 0:
+                neg_passage.append(doc_data[concat_id]["title"] + " " + doc_data[concat_id]["text"])
+                
+        for pos in pos_passage:
+            for neg in neg_passage:
+                save_triplets.append({"question_id": question_id,
+                                      "triplets": (question, pos, neg)})
+                
+                
+    save_path = args.save_path
+    os.makedirs(save_path, exist_ok=True)
+    with open(os.path.join(save_path, f"save_mnrl_triplets_{args.model_name}_top{top_k}.pkl"), "wb") as triplet_file:
+        pickle.dump(save_triplets, triplet_file)
